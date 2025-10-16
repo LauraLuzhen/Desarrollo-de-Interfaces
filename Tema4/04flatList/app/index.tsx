722b1615716501aa250b9f558dd21594fbcdb81c
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-10-16 14:38:22 +0200

    g

--- Tema4/04flatList/app/index.tsx
+++ Tema4/04flatList/app/index.tsx
@@ -1,40 +1,29 @@
-import { Text, StyleSheet, View, FlatList } from "react-native";
-import React, { useState } from "react";
-
-
+import { Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 const usuarios = [
-  { id: 1, nombre: 'Lucía', apellido: 'García'},
-  { id: 2, nombre: 'Lucía', apellido: 'García'},
-  { id: 3, nombre: 'Lucía', apellido: 'García'},
-  { id: 4, nombre: 'Lucía', apellido: 'García'},
-  { id: 5, nombre: 'Lucía', apellido: 'García'}
-];
+  { id: '1', nombre: 'Alvaro', apellido: 'Cabrera' },
+  { id: '2', nombre: 'Isabella', apellido: 'Molina' },
+  { id: '3', nombre: 'Santiago', apellido: 'Vega' },
+  { id: '4', nombre: 'Emilia', apellido: 'Rojas' },
+  { id: '5', nombre: 'Hugo', apellido: 'Torres' },
+  { id: '6', nombre: 'Camila', apellido: 'Figueroa' },
+  { id: '7', nombre: 'Mateo', apellido: 'Silva' },
+  { id: '8', nombre: 'Valeria', apellido: 'Dominguez' },
+  { id: '9', nombre: 'Bruno', apellido: 'Cortes' },
+  { id: '10', nombre: 'Antonella', apellido: 'Herrera' }
+]
 
 export default function Index() {
   return (
     <FlatList
-    data = {usuarios}
-    keyExtractor={(item) => item.id.toString()}
-    renderItem={({item}) => (
-      <View style = {styles.item}>
-        <Text style = {styles.text} >{item.nombre} {item.apellido}</Text>
-      </View>
+    data={usuarios}
+    keyExtractor= {(item) => item.id.toString()}
+    renderItem = {({ item }) => (
+      <View >
+       <Text>{item.nombre} {item. apellido}</Text>
+     </View>
     )}
     />
   )
 }
-
-const styles = StyleSheet.create({
-  item: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderColor: '#d3d3d3ff',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 17
-  }
-});
-
-
